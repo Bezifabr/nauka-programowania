75895cf1487485f0dfa7a000b94b94a24b8e240f
M.Saranda <maciej@saranda.dev>
2022-01-24 21:16:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    String tekst = "Tekst";
+    System.out.println(tekst);
+
+    int i = 10;
+
+    String a = "a";
+    String b = "b";
+
+    String rezultat = a + " " + b;
+    System.out.println(rezultat);
+
+    boolean t = false;
+
+    if(t = true) {
+    System.out.println("true");
+    }
+    if(t = false) {
+    System.out.println("false");
+    }
+
+
+    System.out.println("tekst " + " 10");
+    System.out.println("tekst " + 10);
+    System.out.println(10 + "tekst");
+
+    int x;
+    int y;
+    x = 6;
+    y = 2;
+    int wynik;
+    wynik = x + y;
+    int wynik2;
+    wynik2 = x - y;
+    System.out.println(wynik);
+    System.out.println("wynik: " + x + y);
+    System.out.println("wynik: " + wynik);
+    System.out.println("wynik2: " + wynik2);
+
+    
+
+
+
+
+
+  }
+}
